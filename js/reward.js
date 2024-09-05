6d7a7520e8447d1e1c4d9060f017d9d2ba54e456
YiweiZhang <zywvvd@mail.ustc.edu.cn>
2024-09-05 15:27:30 +0800

    Site updated: 2024-09-05 15:27:29

---
+++ js/reward.js
@@ -0,0 +1,11 @@
+const rewardBtn = document.getElementById('rewardBtn');
+const rewardImgContainer = document.getElementById('rewardImgContainer');
+
+if(rewardBtn){
+	rewardBtn.onclick = () => {
+		rewardImgContainer.style.display = (rewardImgContainer.style.display === 'none' || rewardImgContainer.style.display === '') ? 'inline-flex' : 'none'
+		setTimeout(() => {
+			rewardImgContainer.style.opacity = (rewardImgContainer.style.opacity === '0' || rewardImgContainer.style.opacity === '') ? '1' : '0'
+		}, 10);
+	}
+}
